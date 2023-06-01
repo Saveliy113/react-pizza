a1194682293aec80a44afef906286326fb3d50c3
Saveliy113 <sava2698@gmail.com>
2023-06-01 10:30:29 +0600

    Lazy loading added for routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,51 @@
+import React, { Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
-import Cart from './pages/Cart';
-import NotFound from './pages/NotFound';
-import PizzaDetails from './pages/PizzaDetails';
+// import NotFound from './pages/NotFound';
+// import PizzaDetails from './pages/PizzaDetails';
 
 import './scss/app.scss';
 import MainLayout from './layouts/MainLayout';
 
+const Cart = React.lazy(
+  () => import(/*webpackChunkName: "Cart" */ './pages/Cart')
+);
+const NotFound = React.lazy(
+  () => import(/*webpackChunkName: "NotFound" */ './pages/NotFound')
+);
+const PizzaDetails = React.lazy(
+  () => import(/*webpackChunkName: "PizzaDetails" */ './pages/PizzaDetails')
+);
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
         <Route path="" element={<Home />} />
-        <Route path="cart" element={<Cart />} />
-        <Route path="pizza/:id" element={<PizzaDetails />} />
-        <Route path="*" element={<NotFound />} />
+        <Route
+          path="cart"
+          element={
+            <Suspense fallback={<div>Загрузка...</div>}>
+              <Cart />
+            </Suspense>
+          }
+        />
+        <Route
+          path="pizza/:id"
+          element={
+            <Suspense>
+              <PizzaDetails />
+            </Suspense>
+          }
+        />
+        <Route
+          path="*"
+          element={
+            <Suspense fallback={<div>Загрузка...</div>}>
+              <NotFound />
+            </Suspense>
+          }
+        />
       </Route>
     </Routes>
   );
--- src/pages/PizzaDetails.tsx
+++ src/pages/PizzaDetails.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useEffect } from 'react';
 import axios from 'axios';
-import { useNavigate, useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 import { useState } from 'react';
 
 const PizzaDetails: React.FC = () => {
@@ -43,6 +43,11 @@ const PizzaDetails: React.FC = () => {
       <img src={pizza.imageUrl} alt={pizza.title} />
       <h2>{pizza.title}</h2>
       <h4>{pizza.price} руб.</h4>
+      <Link to='/fdfd'>
+        <button className="button button--outline button--add">
+          <span>Назад</span>
+        </button>
+      </Link>
     </div>
   );
 };
